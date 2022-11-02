8c79ed11b60ad83b3d6dfb236c07b8adcfda4e2b
shubhamthite09 <shubhamthite09@gmail.com>
2022-11-02 11:52:22 +0530

    pelendrome wrong coad

---
+++ pelendrom.js
@@ -0,0 +1,10 @@
+function pelendrom(str){
+    let bag="";
+    for(let i=str.length-1;i>=0;i++){
+        bag+=str[i];
+    }
+    if(str==bag){
+        console.log(true);
+    }
+    console.log(false);
+}
